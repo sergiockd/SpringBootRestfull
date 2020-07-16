9944ade76cedc3de53614b57f0ee5e5077f909e1
Sergio <sergio.santos@opah.com.br>
2020-07-16 14:31:57 -0300

    Atualizacao repo

---
+++ src/main/java/com/ccc/vendas/api/Animal.java
@@ -0,0 +1,5 @@
+package com.ccc.vendas.api;
+
+public interface Animal {
+	void fazerBarulho();
+}
--- src/main/java/com/ccc/vendas/api/MinhaConfiguration.java
+++ src/main/java/com/ccc/vendas/api/MinhaConfiguration.java
@@ -1,8 +1,19 @@
 package com.ccc.vendas.api;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
-@Configuration
+@Development
 public class MinhaConfiguration {
+
+	@Bean
+	public CommandLineRunner executar() {
+		
+		return args -> {
+			
+			System.out.println("Rodando a configuração de desenvolviimento");
+			
+		};
+	}
 	
 }
